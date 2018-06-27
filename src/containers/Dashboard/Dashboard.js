40c973f908e4239e8d4d920092743287badea6ae
Atte Saarinen <atte.saarinen@vincit.fi>
2018-06-27 22:37:40 +0300

    Updade dashboard info

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -4,14 +4,46 @@ import { withRouter } from 'react-router-dom';
 import { signOut } from '../../modules/authentication';
 import Section from '../../components/Section';
 
+const Info = Section.extend`
+  font-size: 0.9rem;
+`;
+
 const Dashboard = (props) => (
   <div>
-    <Section>
-      <p>MM-kisaveikkaus 2018. Klikkaile tulokset "My picks" -näkymässä. Osallistu vetoon maksamalla ennen group stagen veikkausten sulkeutumista mulle (Atelle) Mobile Paylla 20e. Voi pistää myös tippiä, ylimääräset menee hosting-kustannuksiin / lahjotetaan eteenpäin football-data.org API:n ylläpitäjälle.</p>
-      <p>Oikeesta 1x2 tuloksesta 1pts, oikeista maalimääristä 3pts.</p>
-      <p>Payout structure: ???</p>
-      <p>Devaus on vielä vähän kesken, leaderboardit yms. tulee kunhan kerkeen.</p>
-    </Section>
+    <Info>
+      <h3>
+        Mikä?
+      </h3>
+      <p>
+        MM-kisaveikkaus 2018. Klikkaile tulokset "My picks" -näkymässä. Osallistu vetoon maksamalla ennen group stagen veikkausten sulkeutumista mulle (Atelle) Mobile Paylla 20e. Voi pistää myös tippiä, ylimääräset menee hosting-kustannuksiin / lahjotetaan eteenpäin football-data.org API:n ylläpitäjälle.
+      </p>
+
+      <h3>
+        Säännöt
+      </h3>
+      <p>
+        <strong>Lohkovaihe:</strong><br />
+        Oikeesta 1x2 tuloksesta 1pts, oikeista maalimääristä 2pts.<br />
+        <strong>Loput:</strong><br />
+        Oikeesta 1x2 tuloksesta 2pts, oikeista maalimääristä 2pts.
+      </p>
+
+      <h3>
+        Payout structure
+      </h3>
+      <p>
+        <ol>
+          <li>1. 216e</li>
+          <li>2. 108e</li>
+          <li>3. 36e</li>
+        </ol>
+      </p>
+
+      <h3>
+        Muuta
+      </h3>
+      <p>Lisää featureita tulossa sitä tahtia kun kerkeen koodata</p>
+    </Info>
   </div>
 );
 
